1fae634130e302370fd1841391bf8b40e99ceedc
moka <halo.mokanh@gmail.com>
2023-12-07 09:13:18 +0700

    🔧 fix(Maps/index.jsx): import `useMap` from `@vis.gl/react-google-maps` to fix missing dependency error 🔧 fix(Maps/index.jsx): move `API_KEY` import to the top of the file for better organization 🔧 fix(Maps/index.jsx): move `PkgKelurahan` import to the top of the file for better organization 🔧 fix(Maps/index.jsx): import `Box` from `@chakra-ui/react` to fix missing dependency error ✨ feat(Maps/index.jsx): add `MyComponent` to render additional content on the map ✨ feat(Maps/index.jsx): add `id` prop to `Map` component to uniquely identify the map ✨ feat(Maps/index.jsx): add `disableDefaultUI` prop to `Map` component to disable default UI controls ✨ feat(Maps/index.jsx): add `MyComponent` to render additional content on the map

--- src/components/Maps/index.jsx
+++ src/components/Maps/index.jsx
@@ -1,6 +1,23 @@
-import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, useMap } from "@vis.gl/react-google-maps";
+import { useEffect } from "react";
 
 const API_KEY = "AIzaSyALOdn_pNdH0rafCH2XESCX6JRj6rz2YPA";
+import PkgKelurahan from '../../assets/pkg_transportation.json';
+import { Box } from "@chakra-ui/react";
+
+const MyComponent = () => {
+  const map = useMap('one-of-my-maps');
+
+  useEffect(() => {
+    if (!map) return;
+    map.data.addGeoJson(PkgKelurahan)
+  }, [map]);
+  // Do something with the Google Maps map instance
+
+  return <Box>
+    asdajsd
+  </Box>;
+};
 
 const MapsComponent = () => {
   const position = {lat: -2.13212, lng: 106.11366};
@@ -9,12 +26,13 @@ const MapsComponent = () => {
     <APIProvider apiKey={API_KEY}>
       <div style={{ height: "100vh" }}>
         <Map
+          id="one-of-my-maps"
+          disableDefaultUI={true}
           style={{ position: "absolute" }}
           zoom={15}
           center={position}
-        >
-          <Marker position={position}/>
-        </Map>
+        />
+        <MyComponent />
       </div>
     </APIProvider>
   );
