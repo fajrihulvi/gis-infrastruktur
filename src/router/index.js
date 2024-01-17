f77344c51cd816e483a96b9322d46c4cc18d5a58
yumaandhika <yumandhika@gmail.com>
2024-01-17 22:06:50 +0700

    Fix Ignore case

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Home from '../pages/Home';
-import AdminDashboard from '../pages/admin/Dashboard';
-import AdminFormAddUserManagement from '../pages/admin/UserManagement/Form/add';
-import AuthLogin from '../pages/admin/auth/login';
-import AdminFormEditUserManagement from '../pages/admin/UserManagement/Form/edit';
-import AdminInfrastructureCategory from '../pages/admin/InfrastructureCategory';
-import AdminFormAddInfrastructureCategory from '../pages/admin/InfrastructureCategory/Form/add';
-import AdminFormEditInfrastructureCategory from '../pages/admin/InfrastructureCategory/Form/edit';
+import AdminDashboard from '../pages/Admin/Dashboard';
+import AdminFormAddUserManagement from '../pages/Admin/UserManagement/Form/add';
+import AuthLogin from '../pages/Admin/Auth/login';
+import AdminFormEditUserManagement from '../pages/Admin/UserManagement/Form/edit';
+import AdminInfrastructureCategory from '../pages/Admin/InfrastructureCategory';
+import AdminFormAddInfrastructureCategory from '../pages/Admin/InfrastructureCategory/Form/add';
+import AdminFormEditInfrastructureCategory from '../pages/Admin/InfrastructureCategory/Form/edit';
 import UserAuthLogin from '../pages/User/Auth/login';
 import UserAuthRegister from '../pages/User/Auth/register';
-import AdminUserManagement from '../pages/admin/UserManagement';
+import AdminUserManagement from '../pages/Admin/UserManagement';
 
 
 export const routes = [
